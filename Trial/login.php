0b5aa7705178d6fe4230a313cd1c5326f6be3cad
Keanu-Argota <keanu.argota@adamson.edu.ph>
2025-03-28 09:18:42 +0800

    add

---
+++ Trial/login.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Metro District Designs - Commissioned Designs</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            background-color: #E5E4E2;
+            font-family: Arial, sans-serif;
+        }
+
+        .navbar {
+            background-color: #1E1E1E;
+            padding: 10px 0;
+        }
+
+        .navbar-brand {
+            display: flex;
+            align-items: center;
+            color: white !important;
+            font-weight: bold;
+        }
+
+        .navbar-brand img {
+            height: 30px;
+            margin-right: 10px;
+        }
+
+        .navbar-nav {
+            flex-grow: 1;
+            justify-content: center;
+        }
+
+        .navbar-nav .nav-link {
+            color: white !important;
+            text-transform: uppercase;
+            font-weight: bold;
+            margin: 0 10px;
+        }
+
+        .signup-container {
+            background-color: #9b9b9b;
+            width: 800px;
+            padding: 80px;
+            text-align: center;
+            margin: 80px auto;
+        }
+        .signup-container h2 {
+            color: Black;
+            margin-bottom: 20px;
+        }
+        .signup-form input {
+            width: 100%;
+            margin-bottom: 25px;
+            padding: 8px;
+            border: none;
+            box-sizing: border-box;
+        }
+        .signup-form button {
+            width: 100%;
+            padding: 0px;
+            background-color: white;
+            border: none;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container">
+            <a class="navbar-brand" href="Homepage.php">
+                <img src="/api/placeholder/40/40" class="rounded-circle">
+                Metro District Designs
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="Homepage.php">HOME</a></li>
+                    <li class="nav-item"><a class="nav-link" href="Products.php">PRODUCTS</a></li>
+                    <li class="nav-item"><a class="nav-link" href="Commissioned.php">COMMISSIONED DESIGNS</a></li>
+                </ul>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="Signup.php"">SIGNUP</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="signup-container">
+        <h2>LOGIN</h2>
+        <form class="signup-form">
+            <input type="text" placeholder="Name">
+            <input type="email" placeholder="Email">
+            <input type="password" placeholder="Password">
+            <div class="login-link">
+                    Don't have an account <a href="Signup.php">Sign up</a>
+                </div>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
